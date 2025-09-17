b3dd1fac759244b511b8b6fe1f1821018ba8a291
unknown <yuldoshmakhmud1@gmail.com>
2025-09-17 15:42:58 +0900

    firebase

---
+++ lib/screens/orders/orders_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:hgocery_app/widgets/back_widget.dart';
+import 'package:hgocery_app/widgets/empty_screen.dart';
+// ignore: unused_import
+import 'package:provider/provider.dart';
+
+import '../../services/utils.dart';
+import '../../widgets/text_widget.dart';
+import 'orders_widget.dart';
+
+class OrdersScreen extends StatefulWidget {
+  static const routeName = '/OrderScreen';
+
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Utils(context).color;
+    // Size size = Utils(context).getScreenSize;
+    bool _isEmpty = true;
+    return _isEmpty == true
+        ? const EmptyScreen(
+            title: 'You didnt place any order yet',
+            subtitle: 'order something and make me happy :)',
+            buttonText: 'Shop now',
+            imagePath: 'assets/images/cart.png',
+          )
+        : Scaffold(
+            appBar: AppBar(
+              leading: const BackWidget(),
+              elevation: 0,
+              centerTitle: false,
+              title: TextWidget(
+                text: 'Your orders (2)',
+                color: color,
+                textSize: 24.0,
+                isTitle: true,
+              ),
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            ),
+            body: ListView.separated(
+              itemCount: 10,
+              itemBuilder: (ctx, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 2, vertical: 6),
+                  child: OrderWidget(),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider(color: color, thickness: 1);
+              },
+            ),
+          );
+  }
+}
